09b71242089295cdc90e54d4c442d37f7fc04eed
JesusDaniel2023 <130616633+JesusDaniel2023@users.noreply.github.com>
2023-04-20 20:17:01 -0400

    Add files via upload

---
+++ condicionales.py
@@ -0,0 +1,13 @@
+### calculo de imc##
+
+print("calculo del imc")
+print("inicializando programa...")
+peso = float(input("ingresa el valor de tu peso en kg:"))
+altura = float(input("ingresa el valor de tu altura en mts:"))
+
+imc = peso/(altura*altura)
+print("tu valor de imc es igual es igual a", imc)
+1mc < 18.5:
+
+
+
