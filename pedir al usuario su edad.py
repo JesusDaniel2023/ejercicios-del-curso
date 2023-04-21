af45008e91a0b9c467a140b8d135db3e7ee0d322
JesusDaniel2023 <130616633+JesusDaniel2023@users.noreply.github.com>
2023-04-21 11:36:48 -0400

    Add files via upload

---
+++ pedir al usuario su edad.py
@@ -0,0 +1,9 @@
+print("USUARIO")
+
+edad = int(input("Por favor ingresa tu edad:"))
+
+if edad < 18:
+    print("lo sentimos eres menor de edad")
+if edad > 18:
+    print("los datos ingresados son correctos eres mayor de edad")
+    
